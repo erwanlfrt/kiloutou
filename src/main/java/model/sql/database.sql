98078729f4b5f564f5a4bee131f6b717f5ee3db7
Erwan Le Forestier <erwan.leforestier@orange.com>
2021-10-19 10:42:24 +0200

    add sql files

---
+++ src/main/java/model/sql/database.sql
@@ -0,0 +1,142 @@
+DROP TABLE IF EXISTS Loan;
+DROP TABLE IF EXISTS SuitableVehicle;
+DROP TABLE IF EXISTS Computer;
+DROP TABLE IF EXISTS Car;
+DROP TABLE IF EXISTS Bike;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Processor;
+DROP TABLE IF EXISTS GraphicCard;
+DROP TABLE IF EXISTS ComputerAccessory;
+DROP TABLE IF EXISTS VehicleAccessory;
+DROP TABLE IF EXISTS Vehicle;
+DROP TABLE IF EXISTS Equipment;
+
+
+-- #region User
+CREATE TABLE User (
+  mail VARCHAR(100) PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  firstname VARCHAR(30) NOT NULL,
+  login VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL, -- add SHA function
+  address VARCHAR(200),
+  phoneNumber VARCHAR(20)
+);
+
+CREATE TABLE Employee (
+  mail VARCHAR(100) PRIMARY KEY,
+  employeeFunction VARCHAR(30) NOT NULL,
+  employeeService VARCHAR(30) NOT NULL,
+  deskNumber INT,
+  profil enum('ADMIN', 'EQUIPMENT_ADMIN', 'LOAN_ADMIN') NOT NULL,
+  FOREIGN KEY (mail) REFERENCES User(mail)
+);
+-- #endregion User
+
+
+-- #region Equipment
+
+CREATE TABLE Equipment (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  available BOOLEAN DEFAULT 1,
+  imageURL VARCHAR(30)
+);
+
+CREATE TABLE Processor ( 
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  brand VARCHAR(30) NOT NULL,
+  numberOfCores INT,
+  frequency FLOAT
+);
+
+CREATE TABLE GraphicCard (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  brand VARCHAR(30) NOT NULL ,
+  gpu VARCHAR(30),
+  frequency FLOAT
+);
+
+CREATE TABLE ComputerAccessory (
+  id INT PRIMARY KEY,
+  FOREIGN KEY (id) REFERENCES Equipment(id)
+);
+
+CREATE TABLE VehicleAccessory (
+  id INT PRIMARY KEY,
+  FOREIGN KEY (id) REFERENCES Equipment(id)
+);
+
+CREATE TABLE Vehicle (
+  id INT PRIMARY KEY,
+  kilometers INT NOT NULL,
+  renewalKilometers INT NOT NULL,
+  brand VARCHAR(30) NOT NULL,
+  model VARCHAR(30) NOT NULL,
+  registrationNumber VARCHAR(30) NOT NULL,
+  state enum('GOOD', 'AVERAGE', 'BAD'),
+  maxSpeed INT,
+  numberOfSpeeds INT,
+  power INT,
+  FOREIGN KEY (id) REFERENCES Equipment(id)
+);
+
+CREATE TABLE Car (
+  id INT PRIMARY KEY,
+  numberOfSeats INT,
+  FOREIGN KEY (id) REFERENCES Equipment(id)
+);
+
+CREATE TABLE Bike (
+  id INT PRIMARY KEY,
+  numberOfCylinders INT,
+  FOREIGN KEY (id) REFERENCES Equipment(id)
+);
+
+CREATE TABLE SuitableVehicle (
+  accessoryId INT ,
+  vehicleId INT,
+  PRIMARY KEY (accessoryId, vehicleId),
+  FOREIGN KEY (accessoryId) REFERENCES VehicleAccessory(id),
+  FOREIGN KEY (vehicleId) REFERENCES Vehicle(id)
+);
+
+CREATE TABLE Computer (
+  id INT PRIMARY KEY,
+  brand VARCHAR(30) NOT NULL,
+  model VARCHAR(30) NOT NULL,
+  serialNumber VARCHAR(30) NOT NULL,
+  memorySize INT ,
+  isLaptop BOOLEAN NOT NULL,
+  screenSize INT ,
+  purchaseDate DATE NOT NULL,
+  renewalDate DATE NOT NULL,
+  processorId INT NOT NULL,
+  graphicCardId INT NOT NULL,
+  FOREIGN KEY (id) REFERENCES Equipment(id),
+  FOREIGN KEY (processorId) REFERENCES Processor(id),
+  FOREIGN KEY (graphicCardId) REFERENCES GraphicCard(id)
+);
+
+-- #endregion Equipment
+
+
+-- #region Loan
+
+CREATE TABLE Loan (
+  equipmentId INT NOT NULL,
+  userMail VARCHAR(100) NOT NULL,
+  beginningDate DATE NOT NULL,
+  endDate DATE,
+  isBorrowed BOOLEAN,
+  PRIMARY KEY (equipmentId, userMail, beginningDate),
+  FOREIGN KEY (equipmentId) REFERENCES Equipment(id),
+  FOREIGN KEY (userMail) REFERENCES User(mail) 
+);
+
+-- #endregion Loan
+
+
---
+++ src/main/java/model/sql/dump.sql
@@ -0,0 +1,27 @@
+INSERT INTO User (mail, name, firstname, login, password, address, phoneNumber) VALUES ("root@gmail.com", "Bob", "Billy", "root", "toor", "1 root de Bretagne", "0102030405"); 
+
+INSERT INTO Employee (mail, employeeFunction, employeeService, deskNumber, profil) VALUES ("root@gmail.com", "ceo", "administration", 1, "ADMIN");
+
+INSERT INTO Equipment (id, name, available, imageUrl) VALUES (1, "random equipment", 1, "http://randomURL.com");
+
+
+
+INSERT INTO Processor (id, name, brand, numberOfCores, frequency) VALUES (1, "I5", "Intel", 8, 3.2);
+
+INSERT INTO GraphicCard (id, name, brand, gpu, frequency) VALUES (1, "RTX1080", "Nvidia", "Intel I9", 3.5);
+
+INSERT INTO ComputerAccessory (id) VALUES (1);
+
+INSERT INTO VehicleAccessory (id) VALUES (1);
+
+INSERT INTO Vehicle (id, kilometers, renewalKilometers, brand, model, registrationNumber, state, maxSpeed, numberOfSpeeds, power) VALUES (1, 1000, 6000, "Fiat", "500", "AA-000-AA", "GOOD", 180, 5, 80);
+
+INSERT INTO Car (id, numberOfSeats) VALUES (1, 5);
+
+INSERT INTO Bike (id, numberOfCylinders) VALUES (1, 8);
+
+INSERT INTO SuitableVehicle (accessoryId, vehicleId) VALUES (1,1);
+
+INSERT INTO Computer (id, brand, model, serialNumber, memorySize, isLaptop, screenSize, purchaseDate, renewalDate, processorId, graphicCardId) VALUES (1, "Apple", "Macbook", "ax8000xa", 500, true, 13, '2021-05-15', '2025-05-15', 1, 1);
+
+INSERT INTO Loan (equipmentId, userMail, beginningDate, endDate, isBorrowed) VALUES (1, "root@gmail.com", "2021-11-12", "2021-11-30", false);
