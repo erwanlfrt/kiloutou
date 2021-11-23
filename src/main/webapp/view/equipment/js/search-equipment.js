4e445f16dbfe584819857f5190fbdf36057cd4fa
Nathan-Projets <guillouxnathan22gmail.com>
2021-11-23 19:19:22 +0100

    style search-equipment

---
+++ src/main/webapp/view/equipment/js/search-equipment.js
@@ -0,0 +1,43 @@
+/**
+ * search-equipment.js
+ */
+
+document.getElementById("searchBar").addEventListener("keyup", function(ev) {
+	let searchValue = ev.target.value;
+	ev.target.classList.toggle("active", ev.target.value);
+	searchEquipment(searchValue);
+});
+
+document.getElementById("categories").addEventListener("change", function(ev) {
+	filterByCategory(ev.target.value);
+});
+
+function searchEquipment(searchValue) {
+	let equipments = document.getElementsByClassName("list-item");
+	for(let i = 0 ; i < equipments.length ; i++) {
+		let nameEquipment = equipments[i].getElementsByTagName('a')[0].innerText;
+		if(nameEquipment.toLowerCase().includes(searchValue.toLowerCase())) {
+			equipments[i].style.display = "flex";
+		} else {
+			equipments[i].style.display = "none";
+		}
+	}
+}
+
+function filterByCategory(filter) {
+	let list = document.getElementsByClassName("list-equipment")[0].children;
+	for(let i = 0 ; i < list.length ; i += 2) {
+		if(filter.includes("all")) {
+			list[i].style.display = "block";
+			list[i + 1].style.display = "flex";
+		} else {
+			if(list[i + 1].id.toLowerCase().includes(filter.toLowerCase())) {
+				list[i].style.display = "block";
+				list[i + 1].style.display = "flex";
+			} else {
+				list[i].style.display = "none";
+				list[i + 1].style.display = "none";
+			}
+		}
+	}
+}
